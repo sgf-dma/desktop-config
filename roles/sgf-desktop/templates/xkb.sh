bea6c1327687ba9493ac5037a5402e75ef025c76
sgf <sgf.dma@gmail.com>
2021-06-01 18:33:05 +0300

    Part 2.

--- roles/sgf-desktop/templates/xkb.sh
+++ roles/sgf-desktop/templates/xkb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-setxkbmap -I{{ user_xkb_dir }} -rules {{ user_xkb_rules }} {% if xkb_clear_options %} -option {% endif %} -option caps:ctrl_modifier,grp:ctrl_shift_num,ctrl_shift_to_group1  -print | xkbcomp -I{{ user_xkb_dir }} - $DISPLAY
+setxkbmap -I{{ user_xkb_dir }} -rules {{ user_xkb_rules }} {% if _xkb_clear_options %} -option {% endif %} -option caps:ctrl_modifier,grp:ctrl_shift_num,ctrl_shift_to_group1  -print | xkbcomp -I{{ user_xkb_dir }} - $DISPLAY
